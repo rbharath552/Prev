b3e03d647758e516dc54f8981edab6c7579a2499
rbharath552 <rbharath552@gmail.com>
2026-03-21 00:02:30 +0530

    assgn3

--- latest/src/components/Toggle.jsx
+++ latest/src/components/Toggle.jsx
@@ -8,7 +8,7 @@ const Toggle = () => {
     setShow((prev) => !prev);
   };
 return (
-       <div className='bg-amber-200 h-50 w-40  align-items:centre flex  flex-col items-center justify-center'>
+       <div className='bg-amber-300 h-50 w-40  align-items:centre flex  flex-col items-center justify-center'>
       <button className='bg-black text-white' onClick={handleToggle}>
         {show ? "Hide" : "Show"}
       </button >
